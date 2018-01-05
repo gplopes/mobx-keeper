4067baf60a34e7f72cf7910436cb8d508f1a3b34
Gabriel <gabriel@gplopes.com>
2018-01-05 21:51:35 +0100

    add test/checker to localstorage

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,15 +1,39 @@
+/* global window */
 import { reaction, extendObservable } from 'mobx';
 import StorageManager from '../Storage/StorageManager';
 
-// Javascript Utils
+/*
+ * Delay
+ * */
 export const delay = (fn, timer = 300) => setTimeout(fn, timer);
+
+/*
+ * StorageName
+ * Get class instance name and return as string
+ * */
 export const storageName = target => target.constructor.name;
 
-// Mobx Utils
+/*
+ * Test if LocalStorage is available
+ * */
+export const isLocalStorageAvailable = () => {
+  const mod = 'mobxkeeper';
+  try {
+    window.localStorage.setItem(mod, mod);
+    window.localStorage.removeItem(mod);
+    return true;
+  } catch (e) {
+    return false;
+  }
+};
+
+/*
+ *  Mobx Utils
+ * */
 export function trackObservable({ storage, target, key }) {
   return reaction(
     () => target[key],
-    value => StorageManager.update({ storage, key, value }),
+    value => StorageManager.update({ storage, key, value })
   );
 }
 
